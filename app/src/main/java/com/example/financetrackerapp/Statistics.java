3de46971c585953f242444f3e62f35d00f50829f
skeletardii <skeletardii@evil.com>
2024-05-22 14:51:30 +0800

    sql stuff

--- app/src/main/java/com/example/financetrackerapp/Statistics.java
+++ app/src/main/java/com/example/financetrackerapp/Statistics.java
@@ -10,5 +10,7 @@ public class Statistics extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        SQLInterface.createTables();
+        SQLInterface.signup("bascug@email","secret","my name");
     }
 }
