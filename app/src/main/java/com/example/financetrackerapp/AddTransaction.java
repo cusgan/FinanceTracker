1515e845ef3ce81115117d23a207e954502e70d5
X <a@s>
2024-05-29 03:24:58 +0800

    transaction better ui kind of

--- app/src/main/java/com/example/financetrackerapp/AddTransaction.java
+++ app/src/main/java/com/example/financetrackerapp/AddTransaction.java
@@ -2,13 +2,26 @@ package com.example.financetrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddTransaction extends AppCompatActivity {
+    Button btnSaveTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_transaction);
+
+        btnSaveTransaction = (Button) findViewById(R.id.btnSaveTrans);
+        btnSaveTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //do sql stuff
+                finish();
+            }
+        });
     }
 }
