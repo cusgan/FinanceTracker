d69321d2b5c1179dcce5abf67d5bc9f05370fa82
Wens <fmfgbascug@gmail.com>
2024-05-29 18:43:39 +0800

    goal addd buton

--- app/src/main/java/com/example/financetrackerapp/Goals.java
+++ app/src/main/java/com/example/financetrackerapp/Goals.java
@@ -1,5 +1,6 @@
 package com.example.financetrackerapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,7 @@ public class Goals extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    Button btnAddGoal;
 
     public Goals() {
         // Required empty public constructor
@@ -53,6 +56,18 @@ public class Goals extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        btnAddGoal = (Button) getView().findViewById(R.id.btnAddGoalPage);
+        btnAddGoal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        v.getContext(),
+                        Goals.class
+                );
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
